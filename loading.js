748b159b827768d9859073ca395c752a26118f35
LacsadoShaniahSantiago <s10204088@connect.np.edu.sg>
2022-02-17 20:13:22 +0800

    Added loading script. Currrently not working

--- loading.js
+++ loading.js
@@ -53,169 +53,228 @@ const EWT = "https://www.np.edu.sg/lsct/Pages/ewt.aspx"
 const LDH = "https://www.np.edu.sg/lsct/Pages/ldh.aspx"
 const CBE = "https://www.np.edu.sg/lsct/Pages/cbe.aspx"
 
-$(".ba-dip-btn").on("clicked", function (e) {
-    location.href='loading.html';
-    setTimeout(loading, 1000);
+/*Execution Done*/
+//Home Page
+document.getElementsById("quiz-btn").addEventListener("click", Quiz1Page);
 
-})
-$(".de-dip-btn").on("clicked", function (e) {
-    location.href='loading.html';
-    setTimeout(loading, 1000);
+//School Pages
+document.getElementById("CBP").addEventListener("click", CBPPage);
+document.getElementById("ACC").addEventListener("click", ACCPage);
+document.getElementsById("BF").addEventListener("click", BFPage);
+document.getElementsById("BS").addEventListener("click", BSPage);
+document.getElementsById("ITB").addEventListener("click", ITBPage);
+document.getElementsById("TRM").addEventListener("click", TRMPage);
+document.getElementsById("DES").addEventListener("click", DESPage);
+document.getElementsById("HLFM").addEventListener("click", HLFMPage);
+document.getElementsById("CEP").addEventListener("click", CEPPage);
+document.getElementsById("AEG").addEventListener("click", AEGPage);
+document.getElementsById("AMS").addEventListener("click", AMSPage);
+document.getElementsById("BME").addEventListener("click", BMEPage);
+document.getElementsById("EE").addEventListener("click", EEPage);
+document.getElementsById("ECE").addEventListener("click", ECEPage);
+document.getElementsById("ES").addEventListener("click", ESPage);
+document.getElementsById("MOT").addEventListener("click", MOTPage);
+document.getElementsById("ME").addEventListener("click", MEPage);
+document.getElementsById("FSV").addEventListener("click", FSVPage);
+document.getElementsById("MCM").addEventListener("click", MCMPage);
+document.getElementsById("MPP").addEventListener("click", MPPPage);
+document.getElementsById("NSG").addEventListener("click", NSGPage);
+document.getElementsById("OPT").addEventListener("click", OPTPage);
+document.getElementsById("ABM").addEventListener("click", ABMPage);
+document.getElementsById("CMC").addEventListener("click", CMCPage);
+document.getElementsById("CHS").addEventListener("click", CHSPage);
+document.getElementsById("CDT").addEventListener("click", CDTPage);
+document.getElementsById("ECDE").addEventListener("click", ECDEPage);
+document.getElementsById("TSE").addEventListener("click", TSEPage);
+document.getElementsById("CICTP").addEventListener("click", CICTPPage);
+document.getElementsById("CSF").addEventListener("click", CSFPage);
+document.getElementsById("DS").addEventListener("click", DSPage);
+document.getElementsById("IM").addEventListener("click", IMPage);
+document.getElementsById("IT").addEventListener("click", ITPage);
+document.getElementsById("BMS").addEventListener("click", BMSPage);
+document.getElementsById("PHARM").addEventListener("click", PHARMPage);
+document.getElementsById("EWT").addEventListener("click", EWTPage);
+document.getElementsById("LDH").addEventListener("click", LDHPage);
+document.getElementsById("CBE").addEventListener("click", CBEPage);
 
-})
-$(".fms-dip-btn").on("clicked", function (e) {
-    location.href='loading.html';
-    setTimeout(loading, 1000);
+let schoolsrc;
 
-})
-$(".hms-dip-btn").on("clicked", function (e) {
-    location.href='loading.html';
-    setTimeout(loading, 1000);
-
-})
-$(".hs-dip-btn").on("clicked", function (e) {
-    location.href='loading.html';
-    setTimeout(loading, 1000);
-
-})
-$(".ict-dip-btn").on("clicked", function (e) {
-    location.href='loading.html';
-    setTimeout(loading, 1000);
-
-})
-$(".lsct-dip-btn").on("clicked", function (e) {
-    location.href='loading.html';
-    setTimeout(loading, 1000);
-
-})
-$(".soe-dip-btn").on("clicked", function (e) {
-    location.href='loading.html';
-    setTimeout(loading, 1000);
-
-})
-
-let courseBtn = document.getElementsById().onclick;
-
-function source (){
-    if(courseBtn == "CBP"){
-        return CBP;
-    }
-    else if(courseBtn == "ACC"){
-        return ACC;
-    }
-    else if(courseBtn == "BF"){
-        return BF;
-    }
-    else if(courseBtn == "BS"){
-        return BS;
-    }
-    else if(courseBtn == "ITB"){
-        return ITB;
-    }
-    else if(courseBtn == "TRM"){
-        return TRM;
-    }
-    else if(courseBtn == "DES"){
-        return DES;
-    }
-    else if(courseBtn == "HLFM"){
-        return HLFM;
-    }
-    else if(courseBtn == "REB"){
-        return REB;
-    }
-    else if(courseBtn == "CEP"){
-        return CEP;
-    }
-    else if(courseBtn == "AEG"){
-        return AEG;
-    }
-    else if(courseBtn == "AMS"){
-        return AMS;
-    }
-    else if(courseBtn == "BME"){
-        return BME;
-    }
-    else if(courseBtn == "EE"){
-        return EE;
-    }
-    else if(courseBtn == "ECE"){
-        return ECE;
-    }
-    else if(courseBtn == "ES"){
-        return ES;
-    }
-    else if(courseBtn == "MOT"){
-        return MOT;
-    }
-    else if(courseBtn == "ME"){
-        return ME;
-    }
-    else if(courseBtn == "FSV"){
-        return FSV;
-    }
-    else if(courseBtn == "MCM"){
-        return MCM;
-    }
-    else if(courseBtn == "MPP"){
-        return MPP;
-    }
-    else if(courseBtn == "NSG"){
-        return NSG;
-    }
-    else if(courseBtn == "OPT"){
-        return OPT;
-    }
-    else if(courseBtn == "ABM"){
-        return ABM;
-    }
-    else if(courseBtn == "CMC"){
-        return CMC;
-    }
-    else if(courseBtn == "CHS"){
-        return CHS;
-    }
-    else if(courseBtn == "CDT"){
-        return CDT;
-    }
-    else if(courseBtn == "ECDE"){
-        return ECDE;
-    }
-    else if(courseBtn == "TSE"){
-        return TSE;
-    }
-    else if(courseBtn == "CICTP"){
-        return CICTP;
-    }
-    else if(courseBtn == "CSF"){
-        return CSF;
-    }
-    else if(courseBtn == "DS"){
-        return DS;
-    }
-    else if(courseBtn == "IM"){
-        return IM;
-    }
-    else if(courseBtn == "IT"){
-        return IT;
-    }
-    else if(courseBtn == "BMS"){
-        return BMS;
-    }
-    else if(courseBtn == "PHARM"){
-        return PHARM;
-    }
-    else if(courseBtn == "EWT"){
-        return EWT
-    }
-    else if(courseBtn == "LDH"){
-        return LDH;
-    }
-    else if(courseBtn == "CBE"){
-        return CBE;
-    }
+//Home Page
+function Quiz1Page() {
+    schoolsrc = window.open("loading.html", "_self");
+    setTimeout("GoURL('quiz1.html')", 1000);
 }
 
-function loading (){
-    window.location.href = source();
+//School Pages
+function CBPPage() {
+    schoolsrc = window.open("loading.html", "_blank");
+    setTimeout("GoURL('https://www.np.edu.sg/ba/Pages/cbp.aspx')", 1000);
+}
+function ACCPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(ACC)", 1000);
+}
+function BFPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(BF)", 1000);
+}
+function BSPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(BS)", 1000);
+}
+function ITBPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(ITB)", 1000);
+}
+function TRMPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(TRM)", 1000);
+}
+function DESPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(DES)", 1000);
+}
+function HLFMPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(HLFM)", 1000);
+}
+function CEPPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(CEP)", 1000);
+}
+function AEGPage () {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(AEG)", 1000);
+}
+function AMSPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(AMS)", 1000);
+}
+function CBPPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(CBP)", 1000);
+}
+function BMEPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(BME)", 1000);
+}
+function EEPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(EE)", 1000);
+}
+function ECEPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(ECE)", 1000);
+}
+function ESPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(ES)", 1000);
+}
+function MOTPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(MOT)", 1000);
+}
+function MEPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(ME)", 1000);
+}
+function FSVPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(FSV)", 1000);
+}
+function MCMPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(MCM)", 1000);
+}
+function CBPPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(CBP)", 1000);
+}
+function MPPPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(MPP)", 1000);
+}
+function NSGPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(NSG)", 1000);
+}
+function OPTPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(OPT)", 1000);
+}
+function ABMPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(ABM)", 1000);
+}
+function CMCPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(CMC)", 1000);
+}
+function CHSPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(CHS)", 1000);
+}
+function CDTPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(CDT)", 1000);
+}
+function ECDEPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(ECDE)", 1000);
+}
+function TSEPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(TSE)", 1000);
+}
+function CICTPPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(CICTP)", 1000);
+}
+function CSFPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(CSF)", 1000);
+}
+function NSGPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(NSG)", 1000);
+}
+function DSPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(DS)", 1000);
+}
+function IMPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(IM)", 1000);
+}
+function ITPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(IT)", 1000);
+}
+function BMSPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(BMS)", 1000);
+}
+function PHARMPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(PHARM)", 1000);
+}
+function EWTPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(EWT)", 1000);
+}
+function LDHPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(LDH)", 1000);
+}
+function CBEPage() {
+    schoolsrc = LoadPage();
+    setTimeout("GoURL(CBE)", 1000);
+}
+
+function LoadPage() {
+    window.open("loading.html", "_blank");
+}
+
+function GoURL(Url) {
+    schoolsrc.location = Url;
 }
