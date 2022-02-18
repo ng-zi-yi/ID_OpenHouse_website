88be82a91632b970a260117daace26a3093215d2
LacsadoShaniahSantiago <s10204088@connect.np.edu.sg>
2022-02-18 21:23:36 +0800

    Comment out not working loading page. Use for home page quiz button

--- loading.js
+++ loading.js
@@ -1,251 +1,235 @@
-/*Execution Done*/
-let schoolsrc;
+// //Not working
+// /*Execution Done*/
+// //Home Page
+// document.getElementsById("quiz-btn").addEventListener("click", Quiz1Page);
 
-//Home Page
-document.getElementsById("quiz-btn").addEventListener("click", Quiz1Page);
-function Quiz1Page() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoPage('quiz1.html')", 1000);
-}
+// //School Pages
+// document.getElementById("CBP").addEventListener("click", CBPPage);
+// document.getElementById("ACC").addEventListener("click", ACCPage);
+// document.getElementsById("BF").addEventListener("click", BFPage);
+// document.getElementsById("BS").addEventListener("click", BSPage);
+// document.getElementsById("ITB").addEventListener("click", ITBPage);
+// document.getElementsById("TRM").addEventListener("click", TRMPage);
+// document.getElementsById("DES").addEventListener("click", DESPage);
+// document.getElementsById("HLFM").addEventListener("click", HLFMPage);
+// document.getElementsById("REB").addEventListener("click", REBPage);
+// document.getElementsById("CEP").addEventListener("click", CEPPage);
+// document.getElementsById("AEG").addEventListener("click", AEGPage);
+// document.getElementsById("AMS").addEventListener("click", AMSPage);
+// document.getElementsById("BME").addEventListener("click", BMEPage);
+// document.getElementsById("EE").addEventListener("click", EEPage);
+// document.getElementsById("ECE").addEventListener("click", ECEPage);
+// document.getElementsById("ES").addEventListener("click", ESPage);
+// document.getElementsById("MOT").addEventListener("click", MOTPage);
+// document.getElementsById("ME").addEventListener("click", MEPage);
+// document.getElementsById("FSV").addEventListener("click", FSVPage);
+// document.getElementsById("MCM").addEventListener("click", MCMPage);
+// document.getElementsById("MPP").addEventListener("click", MPPPage);
+// document.getElementsById("NSG").addEventListener("click", NSGPage);
+// document.getElementsById("OPT").addEventListener("click", OPTPage);
+// document.getElementsById("ABM").addEventListener("click", ABMPage);
+// document.getElementsById("CMC").addEventListener("click", CMCPage);
+// document.getElementsById("CHS").addEventListener("click", CHSPage);
+// document.getElementsById("CDT").addEventListener("click", CDTPage);
+// document.getElementsById("ECDE").addEventListener("click", ECDEPage);
+// document.getElementsById("TSE").addEventListener("click", TSEPage);
+// document.getElementsById("CICTP").addEventListener("click", CICTPPage);
+// document.getElementsById("CSF").addEventListener("click", CSFPage);
+// document.getElementsById("DS").addEventListener("click", DSPage);
+// document.getElementsById("IM").addEventListener("click", IMPage);
+// document.getElementsById("IT").addEventListener("click", ITPage);
+// document.getElementsById("BMS").addEventListener("click", BMSPage);
+// document.getElementsById("PHARM").addEventListener("click", PHARMPage);
+// document.getElementsById("EWT").addEventListener("click", EWTPage);
+// document.getElementsById("LDH").addEventListener("click", LDHPage);
+// document.getElementsById("CBE").addEventListener("click", CBEPage);
 
-//School Pages
-document.getElementById("CBP").addEventListener("click", CBPPage);
-function CBPPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/ba/Pages/cbp.aspx')", 1000);
-}
+// let schoolsrc;
 
-document.getElementById("ACC").addEventListener("click", ACCPage);
-function ACCPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/ba/Pages/acc.aspx')", 1000);
-}
+// function Quiz1Page() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoPage('quiz1.html')", 1000);
+// }
 
-document.getElementsById("BF").addEventListener("click", BFPage);
-function BFPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/ba/Pages/bf.aspx')", 1000);
-}
+// function CBPPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/ba/Pages/cbp.aspx')", 1000);
+// }
 
-document.getElementsById("BS").addEventListener("click", BSPage);
-function BSPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/ba/Pages/bs.aspx')", 1000);
-}
+// function ACCPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/ba/Pages/acc.aspx')", 1000);
+// }
 
-document.getElementsById("ITB").addEventListener("click", ITBPage);
-function ITBPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/ba/Pages/itb.aspx')", 1000);
-}
+// //For html file
+// function goPage(Url){
+//     schoolsrc.location = Url;
+// }
 
-document.getElementsById("TRM").addEventListener("click", TRMPage);
-function TRMPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/ba/Pages/trm.aspx')", 1000);
-}
+// function BFPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/ba/Pages/bf.aspx')", 1000);
+// }
 
-document.getElementsById("DES").addEventListener("click", DESPage);
-function DESPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/de/Pages/des.aspx')", 1000);
-}
+// function BSPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/ba/Pages/bs.aspx')", 1000);
+// }
 
-document.getElementsById("HLFM").addEventListener("click", HLFMPage);
-function HLFMPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/de/Pages/hlfm.aspx')", 1000);
-}
+// function ITBPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/ba/Pages/itb.aspx')", 1000);
+// }
 
-document.getElementsById("REB").addEventListener("click", REBPage);
-function REBPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/de/Pages/reb.aspx')", 1000);
-}
+// function TRMPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/ba/Pages/trm.aspx')", 1000);
+// }
 
-document.getElementsById("CEP").addEventListener("click", CEPPage);
-function CEPPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/cep.aspx')", 1000);
-}
+// function DESPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/de/Pages/des.aspx')", 1000);
+// }
 
-document.getElementsById("AEG").addEventListener("click", AEGPage);
-function AEGPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/aeg.aspx')", 1000);
-}
+// function HLFMPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/de/Pages/hlfm.aspx')", 1000);
+// }
+// function REBPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/de/Pages/reb.aspx')", 1000);
+// }
+// function CEPPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/cep.aspx')", 1000);
+// }
+// function AEGPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/aeg.aspx')", 1000);
+// }
+// function AMSPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/ams.aspx')", 1000);
+// }
+// function BMEPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/bme.aspx')", 1000);
+// }
+// function EEPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/ee.aspx')", 1000);
+// }
+// function ECEPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/ece.aspx')", 1000);
+// }
+// function ESPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/es.aspx')", 1000);
+// }
+// function MOTPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/mot.aspx')", 1000);
+// }
+// function MEPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/me.aspx')", 1000);
+// }
+// function FSVPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/fms/Pages/fsv.aspx')", 1000);
+// }
+// function MCMPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/fms/Pages/mcm.aspx')", 1000);
+// }
+// function MPPPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/fms/Pages/mpp.aspx')", 1000);
+// }
+// function NSGPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/hs/Pages/nsg.aspx')", 1000);
+// }
+// function OPTPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/hs/Pages/opt.aspx')", 1000);
+// }
+// function ABMPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/hms/Pages/abm.aspx')", 1000);
+// }
+// function CMCPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/hms/Pages/cmc.aspx')", 1000);
+// }
+// function CHSPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/hms/Pages/chs.aspx')", 1000);
+// }
+// function CDTPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/hms/Pages/cdt.aspx')", 1000);
+// }
+// function ECDEPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/hms/Pages/ecde.aspx')", 1000);
+// }
+// function TSEPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/hms/Pages/tse.aspx')", 1000);
+// }
+// function CICTPPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/ict/Pages/cictp.aspx')", 1000);
+// }
+// function CSFPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/ict/Pages/csf.aspx')", 1000);
+// }
+// function DSPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/ict/Pages/ds.aspx')", 1000);
+// }
+// function IMPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/ict/Pages/im.aspx')", 1000);
+// }
+// function ITPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/ict/Pages/it.aspx')", 1000);
+// }
+// function BMSPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/lsct/Pages/bms.aspx')", 1000);
+// }
+// function PHARMPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/lsct/Pages/pharm.aspx')", 1000);
+// }
+// function EWTPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/lsct/Pages/ewt.aspx')", 1000);
+// }
+// function LDHPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/lsct/Pages/ldh.aspx')", 1000);
+// }
+// function CBEPage() {
+//     schoolsrc = window.open("loading.html", "_blank");
+//     setTimeout("GoURL('https://www.np.edu.sg/lsct/Pages/cbe.aspx')", 1000);
+// }
+// //For html pages
+// function GoPage(Url) {
+//     schoolsrc.location = Url;
+// }
+// //For websites
+// function GoURL(Url) {
+//     schoolsrc.location.href = Url;
+// }
 
-document.getElementsById("AMS").addEventListener("click", AMSPage);
-function AMSPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/ams.aspx')", 1000);
-}
+setTimeout(loadPage, 2000);
 
-document.getElementsById("BME").addEventListener("click", BMEPage);
-function BMEPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/bme.aspx')", 1000);
-}
-
-document.getElementsById("EE").addEventListener("click", EEPage);
-function EEPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/ee.aspx')", 1000);
-}
-
-document.getElementsById("ECE").addEventListener("click", ECEPage);
-function ECEPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/ece.aspx')", 1000);
-}
-
-document.getElementsById("ES").addEventListener("click", ESPage);
-function ESPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/es.aspx')", 1000);
-}
-
-document.getElementsById("MOT").addEventListener("click", MOTPage);
-function MOTPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/mot.aspx')", 1000);
-}
-
-document.getElementsById("ME").addEventListener("click", MEPage);
-function MEPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/soe/Pages/me.aspx')", 1000);
-}
-
-document.getElementsById("FSV").addEventListener("click", FSVPage);
-function FSVPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/fms/Pages/fsv.aspx')", 1000);
-}
-
-document.getElementsById("MCM").addEventListener("click", MCMPage);
-function MCMPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/fms/Pages/mcm.aspx')", 1000);
-}
-
-document.getElementsById("MPP").addEventListener("click", MPPPage);
-function MPPPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/fms/Pages/mpp.aspx')", 1000);
-}
-
-document.getElementsById("NSG").addEventListener("click", NSGPage);
-function NSGPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/hs/Pages/nsg.aspx')", 1000);
-}
-
-document.getElementsById("OPT").addEventListener("click", OPTPage);
-function OPTPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/hs/Pages/opt.aspx')", 1000);
-}
-
-document.getElementsById("ABM").addEventListener("click", ABMPage);
-function ABMPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/hms/Pages/abm.aspx')", 1000);
-}
-document.getElementsById("CMC").addEventListener("click", CMCPage);
-function CMCPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/hms/Pages/cmc.aspx')", 1000);
-}
-
-document.getElementsById("CHS").addEventListener("click", CHSPage);
-function CHSPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/hms/Pages/chs.aspx')", 1000);
-}
-
-document.getElementsById("CDT").addEventListener("click", CDTPage);
-function CDTPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/hms/Pages/cdt.aspx')", 1000);
-}
-
-document.getElementsById("ECDE").addEventListener("click", ECDEPage);
-function ECDEPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/hms/Pages/ecde.aspx')", 1000);
-}
-
-document.getElementsById("TSE").addEventListener("click", TSEPage);
-function TSEPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/hms/Pages/tse.aspx')", 1000);
-}
-
-document.getElementsById("CICTP").addEventListener("click", CICTPPage);
-function CICTPPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/ict/Pages/cictp.aspx')", 1000);
-}
-
-document.getElementsById("CSF").addEventListener("click", CSFPage);
-function CSFPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/ict/Pages/csf.aspx')", 1000);
-}
-
-document.getElementsById("DS").addEventListener("click", DSPage);
-function DSPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/ict/Pages/ds.aspx')", 1000);
-}
-
-document.getElementsById("IM").addEventListener("click", IMPage);
-function IMPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/ict/Pages/im.aspx')", 1000);
-}
-
-document.getElementsById("IT").addEventListener("click", ITPage);
-function ITPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/ict/Pages/it.aspx')", 1000);
-}
-document.getElementsById("BMS").addEventListener("click", BMSPage);
-function BMSPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/lsct/Pages/bms.aspx')", 1000);
-}
-
-document.getElementsById("PHARM").addEventListener("click", PHARMPage);
-function PHARMPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/lsct/Pages/pharm.aspx')", 1000);
-}
-
-document.getElementsById("EWT").addEventListener("click", EWTPage);
-function EWTPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/lsct/Pages/ewt.aspx')", 1000);
-}
-document.getElementsById("LDH").addEventListener("click", LDHPage);
-function LDHPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/lsct/Pages/ldh.aspx')", 1000);
-}
-
-document.getElementsById("CBE").addEventListener("click", CBEPage);
-function CBEPage() {
-    schoolsrc = window.open("loading.html", "_blank");
-    setTimeout("GoURL('https://www.np.edu.sg/lsct/Pages/cbe.aspx')", 1000);
-}
-
-//For html file
-function goPage(Url){
-    schoolsrc.location = Url;
-}
-
-//For websites
-function GoURL(Url) {
-    schoolsrc.location.href = Url;
+function loadPage() {
+    window.location = "quiz1.html";
 }
